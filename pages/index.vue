ac48f630954316727f1f803dc2bf5a79bf143709
Vinícius Hoyer <vhoyer@live.com>
2018-10-20 13:05:40 -0300

    create a new / route

---
+++ pages/index.vue
@@ -0,0 +1,44 @@
+<template>
+  <div>
+    <h2>login</h2>
+  </div>
+</template>
+
+<script>
+import { mapState, mapGetters, mapActions } from 'vuex'
+
+export default {
+  data() {
+    return {
+      newCategory: {},
+    }
+  },
+  computed: {
+    ...mapState('wallet', {
+      categories: 'expenses',
+      money: state => state.money.toFixed(2),
+    }),
+  },
+  methods: {
+    ...mapActions('wallet',[
+      'incrementMoney',
+      'addExpense',
+      'incrementExpense',
+    ]),
+    addCategory() {
+      this.addExpense(this.newCategory)
+      this.newCategory = {}
+    },
+    incrementCategory(index, ev) {
+      this.incrementExpense({
+        index,
+        value: ev.target.valueAsNumber
+      })
+      ev.target.value = ''
+    },
+  },
+}
+</script>
+
+<style>
+</style>
